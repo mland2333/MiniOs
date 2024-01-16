9c726eeb0a1b8b191b7a6aa8700b65daa97833dd
mland2333 <2892912401@qq.com>
2024-01-16 16:45:53 +0800

    rv64ima->rv64imac_zicsr_zifencei

--- common.mk
+++ common.mk
@@ -2,7 +2,7 @@ CROSS_COMPILE = riscv64-unknown-elf-
 INC_DIR = $(OS_HOME)/include
 SRC_DIR = $(OS_HOME)/src
 BUILD_DIR = $(OS_HOME)/build
-CFLAGS = -nostdlib -fno-builtin -march=rv64ima -mabi=lp64 -g -Wall -mcmodel=medany -I$(INC_DIR)
+CFLAGS = -nostdlib -fno-builtin -march=rv64imac_zicsr_zifencei -mabi=lp64 -g -Wall -mcmodel=medany -I$(INC_DIR)
 
 QEMU = qemu-system-riscv64
 QFLAGS = -nographic -smp 1 -machine virt -bios none
